cb3e3cda9b1919b522ca274a7a54703b8927fe24
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-08-30 02:28:42 +0530

    ADDED: Functionality to find largest of three floating-point number

--- src/com/bridgelabz/maximumproblem/MaximumProblem.java
+++ src/com/bridgelabz/maximumproblem/MaximumProblem.java
@@ -3,12 +3,37 @@ package com.bridgelabz.maximumproblem;
 public class MaximumProblem {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Maximum Problem Program!");
-		int a = 1, b = 2, c = 3;
-		System.out.println("Maximum of " + a + ", " + b + ", " + c + " is " + findMaximum(a, b, c));
+
+		int integerNumber1 = 1, integerNumber2 = 2, integerNumber3 = 3;		
+		System.out.println("Maximum of " + integerNumber1 + ", " + integerNumber2 + ", " + integerNumber3 + " is " + findMaximum(integerNumber1, integerNumber2, integerNumber3));
+
+		float floatNumber1 = 9.0f, floatNumber2 = 52.0f, floatNumber3 = 3.0f;
+		System.out.println("Maximum of " + floatNumber1 + ", " + floatNumber2 + ", " + floatNumber3 + " is " + findMaximum(floatNumber1, floatNumber2, floatNumber3));
 
 	}
 
 	public static int findMaximum(Integer Number1, Integer Number2, Integer Number3) {
+		if (Number1.compareTo(Number2) > 0) {
+			if (Number1.compareTo(Number3) > 0) {
+				return Number1;
+
+			} else {
+				return Number3;
+			}
+
+		} else {
+			if (Number2.compareTo(Number3) > 0) {
+				return Number2;
+
+			} else {
+				return Number3;
+
+			}
+
+		}
+
+	}
+	public static float findMaximum(Float Number1, Float Number2, Float Number3) {
 		if (Number1.compareTo(Number2) > 0) {
 			if (Number1.compareTo(Number3) > 0) {
 				return Number1;

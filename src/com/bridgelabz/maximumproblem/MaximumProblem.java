a7d41a5e07ed3651b830186e4008445c34536ff1
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-08-30 02:52:19 +0530

    REFACTORED: Find maximum using Generic Class

--- src/com/bridgelabz/maximumproblem/MaximumProblem.java
+++ src/com/bridgelabz/maximumproblem/MaximumProblem.java
@@ -1,20 +1,31 @@
 package com.bridgelabz.maximumproblem;
 
-public class MaximumProblem {
+public class MaximumProblem<E extends Comparable<E>> {
+	E firstValue, secondValue, thirdValue;
+
+	public MaximumProblem(E firstValue, E secondValue, E thirdValue) {
+		this.firstValue = firstValue;
+		this.secondValue = secondValue;
+		this.thirdValue = thirdValue;
+	}
+
+	public void testMaximum()
+	{
+		System.out.println("Maximum of " + this.firstValue + ", " + this.secondValue + ", " + this.thirdValue + " is "
+				+ MaximumProblem.findMaximum(this.firstValue, this.secondValue, this.thirdValue));
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Maximum Problem Program!");
 
 		int integerNumber1 = 1, integerNumber2 = 2, integerNumber3 = 3;
-		System.out.println("Maximum of " + integerNumber1 + ", " + integerNumber2 + ", " + integerNumber3 + " is "
-				+ findMaximum(integerNumber1, integerNumber2, integerNumber3));
+		new MaximumProblem<Integer>(integerNumber1, integerNumber2, integerNumber3).testMaximum();
 
 		float floatNumber1 = 9.0f, floatNumber2 = 52.0f, floatNumber3 = 3.0f;
-		System.out.println("Maximum of " + floatNumber1 + ", " + floatNumber2 + ", " + floatNumber3 + " is "
-				+ findMaximum(floatNumber1, floatNumber2, floatNumber3));
+		new MaximumProblem<Float>(floatNumber1, floatNumber2, floatNumber3).testMaximum();
 
 		String string1 = "hello", string2 = "world", string3 = "computer";
-		System.out.println("Maximum of " + string1 + ", " + string2 + ", " + string3 + " is "
-				+ findMaximum(string1, string2, string3));
+		new MaximumProblem<String>(string1, string2, string3).testMaximum();
 
 	}
 
